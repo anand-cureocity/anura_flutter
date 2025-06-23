f1964436b9f4e0d5ab97bbe8c436b141c50f3aa4
Anand <anand.as@cureo.city>
2025-06-23 13:00:27 +0530

    Add AnuraScannerActivity and update AnuraFlutterPlugin to launch it. Modify AndroidManifest.xml to declare the new activity and add layout for the scanner interface.

--- anura_flutter_android/android/src/main/kotlin/com/example/verygoodcore/AnuraFlutterPlugin.kt
+++ anura_flutter_android/android/src/main/kotlin/com/example/verygoodcore/AnuraFlutterPlugin.kt
@@ -7,9 +7,15 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.content.Intent
+import com.example.verygoodcore.anura.AnuraScannerActivity
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import android.app.Activity
 
-class AnuraFlutterPlugin : FlutterPlugin, MethodCallHandler {
+class AnuraFlutterPlugin : FlutterPlugin, MethodCallHandler,ActivityAware {
     private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "anura_flutter_android")
@@ -18,7 +24,14 @@ class AnuraFlutterPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         if (call.method == "getPlatformName") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")            
+            activity?.let {
+                val intent = Intent(it, AnuraScannerActivity::class.java)
+                it.startActivity(intent)
+                result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            } ?: run {
+                result.error("NO_ACTIVITY", "Activity is null", null)
+            }
+//            result.success("Android ${android.os.Build.VERSION.RELEASE}")
         } else {
             result.notImplemented()
         }
@@ -26,5 +39,22 @@ class AnuraFlutterPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    // ActivityAware methods
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
     }
 }
---
+++ anura_flutter_android/android/src/main/kotlin/com/example/verygoodcore/anura/AnuraScannerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.verygoodcore.anura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.verygoodcore.R
+
+ class AnuraScannerActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+   super.onCreate(savedInstanceState)
+   setContentView(R.layout.anura_scanner)
+  }
+}
